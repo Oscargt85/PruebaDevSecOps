ccbfc201d83f856aa6fa7b385cf103943623efdc
unknown <davidsgt77@gmai.com>
2026-09-09 12:00:41 -0500

    primeros cambios CI/CD, Docker

--- src/main/java/com/miempresa/micro_test/HelloController.java
+++ src/main/java/com/miempresa/micro_test/HelloController.java
@@ -1,4 +1,4 @@
-package com.miempresa.microtest;
+package com.miempresa.micro_test;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
